7ee0c90be8789d5cbad86e2a65986bb0e97c1b7d
CarlosVelasquezAyan <30760621+CarlosVelasquezAyan@users.noreply.github.com>
2022-12-11 12:55:02 -0300

    Added testEstadoMundial

--- src/test/java/com/devopsusach2020/DevOpsUsach2020ApplicationTests.java
+++ src/test/java/com/devopsusach2020/DevOpsUsach2020ApplicationTests.java
@@ -1,12 +1,15 @@
 package com.devopsusach2020;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.devopsusach2020.rest.RestData;
+
+import com.devopsusach2020.model.Mundial;
 
 @SpringBootTest
 class DevOpsUsach2020ApplicationTests {
@@ -21,5 +24,12 @@ class DevOpsUsach2020ApplicationTests {
 	@Test
 	void verifyIfValidaApiCovidHttpIsStatus200_thenOk() {
 		assertTrue(restData.validaApiCovid());
+	}
+
+	@Test 
+	void testEstadoMundial() {
+		Mundial response = restData.getTotalMundial();
+		assertNotNull(response.getTotalConfirmed());
+		assertNotNull(response.getTotalDeaths());
 	}
 }
