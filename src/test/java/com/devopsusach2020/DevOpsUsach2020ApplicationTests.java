6ffd55ac11b109cb3a3f5aadc55cf0565f2f55d2
extronger <diego98810@gmail.com>
2022-12-11 12:57:23 -0300

    test estado pais.

--- src/test/java/com/devopsusach2020/DevOpsUsach2020ApplicationTests.java
+++ src/test/java/com/devopsusach2020/DevOpsUsach2020ApplicationTests.java
@@ -1,11 +1,13 @@
 package com.devopsusach2020;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.devopsusach2020.model.Pais;
 import com.devopsusach2020.rest.RestData;
 
 @SpringBootTest
@@ -21,5 +23,15 @@ class DevOpsUsach2020ApplicationTests {
 	@Test
 	void verifyIfValidaApiCovidHttpIsStatus200_thenOk() {
 		assertTrue(restData.validaApiCovid());
+	}
+
+	@Test
+	void isGetTotalPaisWorking(){
+		String countryToSearch = "Chile";
+		Pais response = restData.getTotalPais(countryToSearch);
+		assertEquals(countryToSearch, response.getCountry());
+		assertEquals("ok", response.getMensaje());
+		assertNotNull(response.getConfirmed());
+		assertNotNull(response.getDeaths());
 	}
 }
